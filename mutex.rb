061d2d0fcd250f3539e1d4af86da0bb5431572fa
Egor Hamaliy <egor.hamaliy@gmail.com>
2014-08-31 23:42:17 +0300

    Mutex sample

---
+++ mutex.rb
@@ -0,0 +1,15 @@
+require 'thread'
+array = []
+
+mutex = Mutex.new
+5.times.map do
+  Thread.new do
+    mutex.synchronize do
+      1000.times do
+        array << nil
+      end
+    end
+  end
+end.each(&:join)
+
+puts array.size
